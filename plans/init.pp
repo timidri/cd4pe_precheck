84117252c6a49d4479c51a44ad13eda79c0212b7
Dimitri Tischenko <dimitri@puppet.com>
2019-10-25 01:37:07 +0200

    renamed check_port plan

--- plans/init.pp
+++ plans/init.pp
@@ -8,7 +8,7 @@ plan cd4pe_precheck(
 # check whether $cd4pe_host exists?
 # ...
 # check connectivity from workstation to $cd4pe_host port 8080, 8443, 443
-run_plan('cd4pe_precheck::check_port',
+run_plan('cd4pe_precheck::check_ports',
   nodes=>localhost,
   remote_host=>$cd4pe_host,
   ports=>[8080,8443,443]
@@ -16,13 +16,13 @@ run_plan('cd4pe_precheck::check_port',
 # check local requirements on $cd4pe_host
 # ...
 # check connectivity from $cd4pe_host to PE master (8140, 8143, 8081, 4433, doublecheck in docs)
-run_plan('cd4pe_precheck::check_port',
+run_plan('cd4pe_precheck::check_ports',
   nodes=>$cd4pe_host,
   remote_host=>$pe_host,
   ports=>[8140, 8143, 8081, 4433]
   )
 # check connectivity from $cd4pe_host to the git server
-run_plan('cd4pe_precheck::check_port',
+run_plan('cd4pe_precheck::check_ports',
   nodes=>$cd4pe_host,
   remote_host=>$git_hostname,
   ports=>[22, 443]
