12bda47de164cf02a39707d5e9527541c9248fa1
Cody Herriges <193064+ody@users.noreply.github.com>
2019-11-24 08:22:46 -0800

    Mostly working port check implementation
    	Includes bash script that does some reasoning about port
    	availability and a validated base plan for checking an array of
    	ports from local and remote host

--- plans/init.pp
+++ plans/init.pp
@@ -1,30 +1,30 @@
 plan cd4pe_precheck(
   TargetSpec $cd4pe_host,
+  TargetSpec $cd4pe_jobs_host,
   TargetSpec $pe_host,
-  String $git_hostname,
-  Optional[TargetSpec] $db_host = $cd4pe_host,
-  Optional[Enum['mysql','postgresql']] $db_type = 'postgresql',
-){
-# check whether $cd4pe_host exists?
-# ...
-# check connectivity from workstation to $cd4pe_host port 8080, 8443, 443
-run_plan('cd4pe_precheck::check_ports',
-  nodes=>localhost,
-  remote_host=>$cd4pe_host,
-  ports=>[8080,8443,443]
+  String     $git_host,
+) {
+  # check connectivity from workstation to $cd4pe_host port 8080, 8443, 443
+  run_plan( 'cd4pe_precheck::ports',
+    remote_host => $cd4pe_host,
+    ports       => [ 8080, 8443, 443 ],
   )
-# check local requirements on $cd4pe_host
-# ...
-# check connectivity from $cd4pe_host to PE master (8140, 8143, 8081, 4433, doublecheck in docs)
-run_plan('cd4pe_precheck::check_ports',
-  nodes=>$cd4pe_host,
-  remote_host=>$pe_host,
-  ports=>[8140, 8143, 8081, 4433]
+  # check connectivity from $cd4pe_host to PE master (8140, 8143, 8081, 4433, doublecheck in docs)
+  run_plan( 'cd4pe_precheck::ports',
+    root_host   => $cd4pe_host,
+    remote_host => $pe_host,
+    ports       => [ 8170, 8140, 8143, 8081, 4433 ]
   )
-# check connectivity from $cd4pe_host to the git server
-run_plan('cd4pe_precheck::check_ports',
-  nodes=>$cd4pe_host,
-  remote_host=>$git_hostname,
-  ports=>[22, 443]
+  # check connectivity from $cd4pe_host and $cd4pd_jobs_host to the git server
+  run_plan( 'cd4pe_precheck::ports',
+    root_host   => [ $cd4pe_host, $cd4pe_jobs_host ],
+    remote_host => $git_hostname,
+    ports       => [ 22, 443 ],
+  )
+  # check connectivity from $cd4pe_jobs_host to $cd4pd_host
+  run_plan( 'cd4pe_precheck::ports',
+    root_host   => $cd4pe_jobs_host,
+    remote_host => $cd4pe_host,
+    ports       => [ 8000, 8080, 8443, 7000 ],
   )
 }
--- tasks/check_port.sh
+++ tasks/check_port.sh
@@ -1,4 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-echo Task: $checking $PT_port on $PT_host
-curl -s $PT_host:$PT_port >/dev/null && echo Connected. || echo Fail
+output=$(curl -s -S --connect-timeout 1 $PT_host:$PT_port 2>&1)
+value=$(echo $?)
+case $value in
+    56) 
+        if [[ "${PT_port}" == '22' ]]; then
+            echo "OK: SSH port $PT_port likely available on $PT_host"; exit 0
+        fi
+    ;;
+    0)
+        echo "WARNING: Port $PT_port likely occupied by running service on $PT_host"; exit 2
+    ;;
+    7)
+        if [[ "${output}" =~ ^.*Connection\ refused$ ]]; then
+          if [[ "${PT_port}" == '22' ]]; then
+            echo "FAIL: $PT_host not listening for SSH at $PT_port"; exit 3
+          fi
+          echo "OK: Port $PT_port likely open and unoccupied on $PT_host"; exit 0
+        else
+          echo "FAIL: Port $PT_port likely blocked to $PT_host"; exit 3
+        fi
+    ;;
+    6)
+        echo "FAIL: DNS error resolving $PT_host"; exit 3
+    ;;
+    *)
+        echo "ERROR: Unexpected results testing $PT_host for $PT_port"; exit 1
+    esac
